7043be4cd99a5d61af7214e56087e367b23472ae
Robert Henderson <balamkej@gmail.com>
2017-05-16 11:08:20 -0700

    added truth values

--- BCBL.hs
+++ BCBL.hs
@@ -12,9 +12,10 @@ reduce K = K
 reduce (Ap (Ap K x) y) = reduce x
 reduce (Ap (Ap (Ap S x) y) z) = reduce (Ap (Ap x z) (Ap y z))
 reduce (Ap x y) | (reduce x) == x && (reduce y) == y = (Ap x y) 
-                 {-- We stop reduction when it has no effect, allowing 
-                 the computation to terminate (e.g., when x and y are K or S) --}  
-                 | otherwise = reduce (Ap (reduce x) (reduce y))
+                | otherwise = reduce (Ap (reduce x) (reduce y))
+                {-- We stop reduction when it has no effect, allowing 
+                the computation to terminate (e.g., when x and y are K or S) --}
 
---T = K
---F = Ap S K
+--Truth Values
+t = K
+f = Ap S K
