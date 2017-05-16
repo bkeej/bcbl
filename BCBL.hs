954ef7930d2a443f00c7f8e67b474068be0e6d90
Robert Henderson <balamkej@gmail.com>
2017-05-15 19:03:02 -0700

    replaced Con with Ap for 'apply'

--- BCBL.hs
+++ BCBL.hs
@@ -1,16 +1,16 @@
-data Term =  S | K | Con Term Term
+data Term =  S | K | Ap Term Term
     deriving (Eq)
 
 instance Show Term where
-    show (Con x y) = "(" ++ show x ++ show y ++ ")"
+    show (Ap x y) = "(" ++ show x ++ show y ++ ")"
     show S = "S"
     show K = "K"
 
 reduce :: Term -> Term
 reduce S = S
 reduce K = K 
-reduce (Con (Con K x) y) = reduce x
-reduce (Con (Con (Con S x) y) z) = reduce (Con (Con x z) (Con y z))
-reduce (Con x y) | (reduce x) == x && (reduce y) == y = (Con x y) 
+reduce (Ap (Ap K x) y) = reduce x
+reduce (Ap (Ap (Ap S x) y) z) = reduce (Ap (Ap x z) (Ap y z))
+reduce (Ap x y) | (reduce x) == x && (reduce y) == y = (Ap x y) 
                  {-- We stop reduceuation when it accomplishes nothing, allowing                     the computation to terminate (e.g., when x and y are K or S) --}  
-                 | otherwise = reduce (Con (reduce x) (reduce y))
+                 | otherwise = reduce (Ap (reduce x) (reduce y))
